5ab581b9c00e1049a36a02118afb8b66f9c6c6e7
Simran Farrukh <84728384+simranfarrukh@users.noreply.github.com>
2021-12-16 12:49:24 -0500

    Update tfidf.py

--- tfidf.py
+++ tfidf.py
@@ -3,8 +3,9 @@
 # @author Simran
 # @version 1.0
 
-import pandas as pd
-import numpy as np
+import pandas as pd  # analyse data
+import numpy as np  # for working with friends
+# machine learning library
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
